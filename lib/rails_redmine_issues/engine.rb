bfc921dce19ba4455c251c95d7274d61d1f045bf
Русских Артем <rusartx@gmail.com>
2013-11-01 05:03:05 +0600

    engine

--- lib/rails_redmine_issues/engine.rb
+++ lib/rails_redmine_issues/engine.rb
@@ -1,7 +1,7 @@
 module RailsRedmineIssues
 
   class Engine < Rails::Engine
-	initialize "rails_redmine_issues.load_app_instance_data" do |app|
+	initializer "rails_redmine_issues.load_app_instance_data" do |app|
       RailsRedmineIssues.setup do |config|
         config.app_root = app.root
       end
