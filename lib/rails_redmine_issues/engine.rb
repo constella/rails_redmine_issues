a49abc87c80915cb362d8d0ae8119efa6279c9d3
Русских Артем <rusartx@gmail.com>
2013-11-01 04:23:10 +0600

    routes didn't work in rubygems

--- lib/rails_redmine_issues/engine.rb
+++ lib/rails_redmine_issues/engine.rb
@@ -1,6 +1,15 @@
 module RailsRedmineIssues
 
   class Engine < Rails::Engine
+	initialize "rails_redmine_issues.load_app_instance_data" do |app|
+      RailsRedmineIssues.setup do |config|
+        config.app_root = app.root
+      end
+    end
+
+    initialize "rails_redmine_issues.load_static_assets" do |app|
+      app.middleware.use ::ActionDispatch::Static, "#{root}/public"
+    end
 
   end
 
--- lib/rails_redmine_issues/version.rb
+++ lib/rails_redmine_issues/version.rb
@@ -1,3 +1,3 @@
 module RailsRedmineIssues
-  VERSION = "0.0.3"
+  VERSION = "0.0.4"
 end
